f044e07b2467b8d53f7a21b20f1809e103188fab
Allan Neto <allan.netto@hotmail.com>
2020-07-12 13:11:19 -0300

    Load de Cards e estilização finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from "./styles/global";
 
-import Routes from './routes';
+import { BrowserRouter } from "react-router-dom";
+
+import Routes from "./routes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle></GlobalStyle>
+    </>
   );
 }
 
